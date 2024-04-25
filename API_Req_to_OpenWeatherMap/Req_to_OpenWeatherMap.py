a572ec8afda384cb8dd1492182937eddde2c88eb
KingHittz <133883048+KingHittz@users.noreply.github.com>
2024-04-25 15:17:31 -0500

    Add files via upload

---
+++ API_Req_to_OpenWeatherMap/Req_to_OpenWeatherMap.py
@@ -0,0 +1,30 @@
+#Made by. Justin Turner 11:33AM 4/25/24
+
+import datetime as datetime
+import requests
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+API_KEY = 'b9f3269290ef5f9e7de33a3db53afbf6'
+CITY = 'Manchester'
+
+def kelvin_to_celsius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
+url = BASE_URL + 'appid=' + API_KEY + '&q=' + CITY
+
+response = requests.get(url).json()
+
+temp_kelvin = response['main']['temp']
+temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+feels_like_kelvin = response['main']['feels_like']
+feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
+wind_speed = response['wind']['speed']
+humidity = response['main']['humidity']
+description = response['weather'][0]['description']
+sunrise_time = datetime.datetime.fromtimestamp(response['sys']['sunrise'] + response['timezone'])
+sunset_time = datetime.datetime.fromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+print(f'Sun rises in {CITY} at {sunrise_time}) local time.')
+print(f'Sun sets in {CITY} at {sunset_time}) local time.')
+print(f'Wind speed in {CITY}: {wind_speed}m/s')
