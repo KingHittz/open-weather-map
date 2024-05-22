d35d4f22977c77d94357bab110bd7a8b6b4eab79
KingHittz <turneraaronjustin@gmail.com>
2024-05-21 19:16:42 -0500

    restructured

--- API_Req_to_OpenWeatherMap/main.py
+++ API_Req_to_OpenWeatherMap/main.py
@@ -1,36 +1,57 @@
 #Made by. Justin Turner 11:33AM 4/25/24
 
-import datetime as datetime
-
+import datetime
 import requests
 
-BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+class WeatherData:
+    def __init__(self, api_key, city, state):
+        self.api_key = api_key
+        self.city = city
+        self.state = state
+        self.base_url = 'https://api.openweathermap.org/data/2.5/weather?'
 
-API_KEY = 'INSERT_API_HERE'
+    def get_weather(self):
+        url = f"{self.base_url}appid={self.api_key}&q={self.city}"
+        response = requests.get(url).json()
+        return self.pull_data(response)
 
-CITY = 'INSERT_CITY_HERE'
+    def pull_data(self, data):
+        temp_kelvin = data['main']['temp']
+        feels_like_kelvin = data['main']['feels_like']
+        wind_speed = data['wind']['speed']
+        humidity = data['main']['humidity']
+        description = data['weather'][0]['description']
+        sunrise_time = datetime.datetime.fromtimestamp(data['sys']['sunrise'] + data['timezone'])
+        sunset_time = datetime.datetime.fromtimestamp(data['sys']['sunset'] + data['timezone'])
+        return {
+            'temp_kelvin': temp_kelvin,
+            'feels_like_kelvin': feels_like_kelvin,
+            'wind_speed': wind_speed,
+            'humidity': humidity,
+            'description': description,
+            'sunrise_time': sunrise_time,
+            'sunset_time': sunset_time
+        }
 
-STATE = 'INSERT_STATE_HERE'
+    def convert_kelvin_to_celsius_fahrenheit(self, kelvin):
+        celsius = kelvin - 273.15
+        fahrenheit = celsius * (9/5) + 32
+        return celsius, fahrenheit
 
-def kelvin_to_celsius_fahrenheit(kelvin):
-    celsius = kelvin - 273.15
-    fahrenheit = celsius * (9/5) + 32
-    return celsius, fahrenheit
+def main():
+    api_key = 'INSERT_API'  # Replace with your actual API key
+    city = 'INSERT_CITY'
+    state = 'INSERT_STATE'
 
-url = BASE_URL + 'appid=' + API_KEY + '&q=' + CITY
+    weather = WeatherData(api_key, city, state)
+    data = weather.get_weather()
 
-response = requests.get(url).json()
+    celsius, fahrenheit = weather.convert_kelvin_to_celsius_fahrenheit(data['temp_kelvin'])
 
-temp_kelvin = response['main']['temp']
-temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
-feels_like_kelvin = response['main']['feels_like']
-feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
-wind_speed = response['wind']['speed']
-humidity = response['main']['humidity']
-description = response['weather'][0]['description']
-sunrise_time = datetime.datetime.fromtimestamp(response['sys']['sunrise'] + response['timezone'])
-sunset_time = datetime.datetime.fromtimestamp(response['sys']['sunset'] + response['timezone'])
+    print(f'Sun rises in {city}, {state} at {data["sunrise_time"]}) local time.')
+    print(f'Sun sets in {city}, {state} at {data["sunset_time"]}) local time.')
+    print(f'Wind speed in {city}, {state}: {data["wind_speed"]} m/s')
+    # You can add similar print statements for other data points
 
-print(f'Sun rises in {CITY, STATE} at {sunrise_time}) local time.')
-print(f'Sun sets in {CITY,STATE} at {sunset_time}) local time.')
-print(f'Wind speed in {CITY, STATE}: {wind_speed}m/s')
+main()
+
